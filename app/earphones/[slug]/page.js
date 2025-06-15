7d6980850f7ff55f99b31e2d7363d9f78eeb369a
samuelaidoo45 <samuelaidoo45@gmail.com>
2025-06-15 23:34:48 +0000

    Add generateStaticParams to dynamic product pages for static export

--- app/earphones/[slug]/page.js
+++ app/earphones/[slug]/page.js
@@ -1,4 +1,12 @@
 import ProductDetails from '../../../components/ProductDetails';
+import data from '../../../data/data.json';
+
+export async function generateStaticParams() {
+  const earphones = data.filter(product => product.category === 'earphones');
+  return earphones.map((product) => ({
+    slug: product.slug,
+  }));
+}
 
 export default async function EarphoneProduct({ params }) {
   const { slug } = await params;
--- app/speakers/[slug]/page.js
+++ app/speakers/[slug]/page.js
@@ -1,4 +1,12 @@
 import ProductDetails from '../../../components/ProductDetails';
+import data from '../../../data/data.json';
+
+export async function generateStaticParams() {
+  const speakers = data.filter(product => product.category === 'speakers');
+  return speakers.map((product) => ({
+    slug: product.slug,
+  }));
+}
 
 export default async function SpeakerProduct({ params }) {
   const { slug } = await params;
